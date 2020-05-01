733122b0b4ce59f1d96ca5c4805f43ca867c7f57
GhostX <egamovkhurshed29@gmail.com>
2020-05-01 18:38:22 +0500

    MyWorld has been added

--- Program.cs
+++ Program.cs
@@ -15,72 +15,8 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Black;
-            List<MyWorld> li = new List<MyWorld>();
-            while (true)
-            {
-                Task.Run(() =>
-                {
-                    for (int i = 0; i < new Random().Next(100, 1000); i++)
-                    {
-                        li.Add(new MyWorld()
-                        {
-                            Left = new Random().Next(100),
-                            world = World(),
-                            Top = new Random().Next(2, 8)
-                        });
-                    }
-                }).Wait();
-                Task.Run(() =>
-                {
-                    Show(li);
-                    li.Clear();
-                }).Wait();
-            }
+            
         }
-        static char[] World()
-        {
-            string world = "";
-            for (char i = 'a'; i <= 'z'; i++)
-            {
-                if (new Random().Next(0, 4) == 1)
-                {
-                    world += i;
-                }
-            }
-            return world.ToCharArray();
-        }
-        static void Show(List<MyWorld> symbols)
-        {
-            int w = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                foreach (var x in symbols)
-                {
-                    Console.CursorTop = i + x.Top;
-                    foreach (var z in x.world)
-                    {
-                        Console.CursorLeft = x.Left;
-                        if (w == x.world.Length - 1)
-                        {
-                            Console.ForegroundColor = ConsoleColor.White;
-                        }
-                        else if (w == x.world.Length - 2)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        }
-                        Console.WriteLine(z);
-                        w++;
-                    }
-                    w = 0;
-                }
-                Thread.Sleep(500);
-                Console.Clear();
-            }
-        }
+        
     }
 }
