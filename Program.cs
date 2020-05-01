dd993be9a17a712ced6237533bd676d5532f44ca
GhostX <egamovkhurshed29@gmail.com>
2020-05-01 18:39:07 +0500

    Method Show added

--- Program.cs
+++ Program.cs
@@ -18,5 +18,37 @@ namespace Matrix
             
         }
         
+        static void Show(List<MyWorld> symbols)
+        {
+            int w = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (var x in symbols)
+                {
+                    Console.CursorTop = i + x.Top;
+                    foreach (var z in x.world)
+                    {
+                        Console.CursorLeft = x.Left;
+                        if (w == x.world.Length - 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if (w == x.world.Length - 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        }
+                        Console.WriteLine(z);
+                        w++;
+                    }
+                    w = 0;
+                }
+                Thread.Sleep(500);
+                Console.Clear();
+            }
+        }
     }
 }
